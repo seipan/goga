571c75d0f8757ca1c11a29ab1e7c9cdbc5ae7b37
seipan <yamasakipann0218@gmail.com>
2024-06-28 00:16:58 +0900

    feat : csv Print

--- internal/printer/fmt.go
+++ internal/printer/fmt.go
@@ -7,3 +7,5 @@ type Fmt struct{}
 func (p Fmt) Print(fitness float64, fitnessCount uint) {
 	fmt.Printf("%.3f, %3d\n", fitness, fitnessCount)
 }
+
+func (p Fmt) Close() {}
--- printer.go
+++ printer.go
@@ -2,4 +2,5 @@ package goga
 
 type Printer interface {
 	Print(fitness float64, fitnessCount uint)
+	Close()
 }
